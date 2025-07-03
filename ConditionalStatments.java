d9beb1b69c4ffee68b16ac70ff76006e0fb14fc7
vijayraaj22122000 <157813045+vijayraaj22122000@users.noreply.github.com>
2025-07-03 17:19:24 +0530

    Add files via upload

---
+++ ConditionalStatments.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class ConditionalStatments {
+
+	public static void main(String[] args) {
+		int age=18;
+		if(age<=18) {
+			System.out.println("Eligible for vote");
+		}else {
+			System.out.println("Not eligible for vote");
+		}
+
+	}
+
+}
